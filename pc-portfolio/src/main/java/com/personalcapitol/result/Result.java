f07a0d8f055be285e28baca963a4e6cbce2acc3a
Ajay Chandra Somepalli <ajay.somepalli@gmail.com>
2020-01-05 09:03:44 -0800

    First commit

---
+++ pc-portfolio/src/main/java/com/personalcapitol/result/Result.java
@@ -0,0 +1,28 @@
+package com.personalcapitol.result;
+
+public class Result {
+
+	private double median;
+	private double bestCase;
+	private double worstCase;
+	
+	public double getMedian() {
+		return median;
+	}
+	public void setMedian(double median) {
+		this.median = median;
+	}
+	public double getBestCase() {
+		return bestCase;
+	}
+	public void setBestCase(double bestCase) {
+		this.bestCase = bestCase;
+	}
+	public double getWorstCase() {
+		return worstCase;
+	}
+	public void setWorstCase(double worstCase) {
+		this.worstCase = worstCase;
+	}
+	
+}
